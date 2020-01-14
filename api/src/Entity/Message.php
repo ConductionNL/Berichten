cda6e3f8dfe32232329cbf7b8d77359cc4d82b5f
Ruben van der Linde <ruben@conduction.nl>
2020-01-14 07:12:37 +0100

    Updated with entities

---
+++ api/src/Entity/Message.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\UuidInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+use App\Entity\Service;
+
+/**
+ * A  message to be send to a spefic recipient or list troug a message service
+ * 
+ * @ApiResource(
+ *     normalizationContext={"groups"={"read"}, "enable_max_depth"=true},
+ *     denormalizationContext={"groups"={"write"}, "enable_max_depth"=true}
+ * )
+ * @ORM\Entity(repositoryClass="App\Repository\MessageRepository")
+ */
+class Message
+{
+	/**
+	 * @var UuidInterface The UUID identifier of this resource
+	 *
+	 * @example e2984465-190a-4562-829e-a8cca81aa35d
+	 *
+	 * @Assert\Uuid
+	 * @Groups({"read"})
+	 * @ORM\Id
+	 * @ORM\Column(type="uuid", unique=true)
+	 * @ORM\GeneratedValue(strategy="CUSTOM")
+	 * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+	 */
+	private $id;
+
+    /**
+     * @var string Iether a contact component person or contact list that will recieve this message
+	 * @example https://cc.zaakonline.nl/people/06cd0132-5b39-44cb-b320-a9531b2c4ac7
+     * 
+     * @Assert\Url
+     * @Assert\Length(
+     *      max = 255
+     * )
+     * @Groups({"read", "write"})
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $reciever;
+
+    /**
+     * @var string Iether a contact component person, or wrc application that sends this message
+	 * @example https://cc.zaakonline.nl/people/06cd0132-5b39-44cb-b320-a9531b2c4ac7
+     *      
+     * @Assert\Url
+     * @Assert\Length(
+     *      max = 255
+     * )
+     * @Groups({"read", "write"})
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $sender;
+
+    /**
+     * @var string The webresource template object (from wrc) that is used as content for this message
+	 * @example https://wrc.zaakonline.nl/templates/013276cc-1483-46b4-ad5b-1cba5acf6d9f
+     * 
+     * @Assert\Url
+     * @Assert\Length(
+     *      max = 255
+     * )
+     * @Groups({"read", "write"})
+     * @ORM\Column(type="string", length=255)
+     */
+    private $content;
+
+    /**
+     * @var string The current status of this message
+	 * @example concept
+	 * 
+     * @Assert\Choice({"concept", "queued", "sending", "send", "delivered"})
+	 * @Assert\Length(
+	 *      max = 255
+	 * )
+	 * 
+     * @Groups({"read", "write"})
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @var \Service The webresource template object (from wrc) that is used as content for this message
+     * 
+     * @Groups({"read", "write"})
+     * @MaxDepth(1)
+     * @ORM\ManyToOne(targetEntity="App\Entity\Service", inversedBy="messages")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $service;
+
+    /**
+     * @var string The data that is used to render the template
+     * 
+     * @Groups({"read", "write"})
+     * @Assert\Json
+     * @ORM\Column(type="json", nullable=true)
+     */
+    private $data = [];
+    
+    public function getId(): ?string
+    {
+    	return $this->id;
+    }
+    
+    public function setId(string $id): self
+    {
+    	$this->id = $id;
+    	
+    	return $this;
+    }
+
+    public function getReciever(): ?string
+    {
+        return $this->reciever;
+    }
+
+    public function setReciever(?string $reciever): self
+    {
+        $this->reciever = $reciever;
+
+        return $this;
+    }
+
+    public function getSender(): ?string
+    {
+        return $this->sender;
+    }
+
+    public function setSender(?string $sender): self
+    {
+        $this->sender = $sender;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getService(): ?Service
+    {
+        return $this->service;
+    }
+
+    public function setService(?Service $service): self
+    {
+        $this->service = $service;
+
+        return $this;
+    }
+
+    public function getData(): ?array
+    {
+        return $this->data;
+    }
+
+    public function setData(?array $data): self
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+}
