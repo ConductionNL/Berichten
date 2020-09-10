66f6e8b3510ac5b074ee3c7e9398d691068e2afa
Robert Zondervan <rjzondervan@users.noreply.github.com>
2020-09-10 07:17:09 +0000

    Apply fixes from StyleCI

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -76,7 +76,7 @@ class AppFixtures extends Fixture
             $id = Uuid::fromString('1541d15b-7de3-4a1a-a437-80079e4a14e0');
             $service = new Service();
             $service->setType('mailer');
-            $service->setOrganization($this->commonGroundService->cleanUrl(['component'=>'wrc','type'=>'organizations','id'=>'d280c4d3-6310-46db-9934-5285ec7d0d5e']));
+            $service->setOrganization($this->commonGroundService->cleanUrl(['component'=>'wrc', 'type'=>'organizations', 'id'=>'d280c4d3-6310-46db-9934-5285ec7d0d5e']));
             $service->setAuthorization('mailgun+api://!changeme!:mail.westfriesland.commonground.nu@api.eu.mailgun.net');
             $manager->persist($service);
             $service->setId($id);
