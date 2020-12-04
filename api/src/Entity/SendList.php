977c7e7a1268cbe87f67c0f372217e99ec439b35
Wilco Louwerse <wilco@conduction.nl>
2020-12-04 11:33:56 +0100

    Added apifilter organization for sendList

--- api/src/Entity/SendList.php
+++ api/src/Entity/SendList.php
@@ -33,7 +33,10 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ApiFilter(BooleanFilter::class)
  * @ApiFilter(OrderFilter::class)
  * @ApiFilter(DateFilter::class, strategy=DateFilter::EXCLUDE_NULL)
- * @ApiFilter(SearchFilter::class, properties={"claim.id": "exact"})
+ * @ApiFilter(SearchFilter::class, properties={
+ *     "claim.id": "exact",
+ *     "organization": "exact"
+ * })
  */
 class SendList
 {
