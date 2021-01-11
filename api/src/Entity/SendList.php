eadbe6294b1d4856a9d9a5175da246b710a6580c
Wilco Louwerse <wilco@conduction.nl>
2021-01-11 15:29:05 +0100

    Added correct apifilters for sendLists

--- api/src/Entity/SendList.php
+++ api/src/Entity/SendList.php
@@ -34,8 +34,8 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ApiFilter(OrderFilter::class)
  * @ApiFilter(DateFilter::class, strategy=DateFilter::EXCLUDE_NULL)
  * @ApiFilter(SearchFilter::class, properties={
- *     "claim.id": "exact",
- *     "organization": "exact"
+ *     "organization": "exact",
+ *     "resource": "exact"
  * })
  */
 class SendList
